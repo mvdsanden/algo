6cf2be4594e2e7984724f23e67e7ab82a872b505
Marten van de Sanden <marten.v@samsung.com>
2015-06-29 14:09:08 -0400

    Initial Commit

---
+++ Algo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Algo
+//
+//  Created by Marten van de Sanden on 6/29/15.
+//  Copyright (c) 2015 Marten van de Sanden. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
