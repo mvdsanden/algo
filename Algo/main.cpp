594c49b387a8b83ee20e2749448b0a61b34fd99a
Marten van de Sanden <marten.v@samsung.com>
2015-06-30 17:06:30 -0400

    Radix sort

--- Algo/main.cpp
+++ Algo/main.cpp
@@ -8,8 +8,41 @@
 
 #include <iostream>
 
+#include "LibCSorter.h"
+#include "MergeSorter.h"
+#include "QuickSorter.h"
+#include "BinaryHeapSorter.h"
+#include "CountingSorter.h"
+#include "RadixSorter.h"
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+    size_t length = 21;
+    
+    std::cout << "LibCSort:\n";
+    me::LibCSorter s0; s0.test(length);
+    
+    std::cout << "MergeSort:\n";
+    me::MergeSorter s1; s1.test(length);
+
+    std::cout << "QuickSort:\n";
+    me::QuickSorter s2; s2.test(length);
+
+    std::cout << "BinaryHeapSort:\n";
+    me::BinaryHeapSorter s3; s3.test(length);
+
+    std::cout << "CountingSort:\n";
+    me::CountingSorter s4; s4.test(length);
+
+    std::cout << "RadixSort:\n";
+    me::RadixSorter s5; s5.test(length);
+    
+    s0.bench(10000000, 1);
+    s1.bench(10000000, 1);
+    s2.bench(10000000, 1);
+    s3.bench(10000000, 1);
+    s4.bench(10000000, 1);
+    s5.bench(10000000, 1);
+    
     return 0;
 }
